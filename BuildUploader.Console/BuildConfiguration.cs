c9a5c9c376cf5b614d82b0a94328df7293b1dcd8
Rainer Angermann <rainer@rarebyte.com>
2019-01-26 16:34:15 +0100

    Change Code Formatting and Fix Typos

--- BuildUploader.Console/BuildConfiguration.cs
+++ BuildUploader.Console/BuildConfiguration.cs
@@ -1,13 +1,11 @@
 ﻿using Newtonsoft.Json;
 
-namespace BuildUploader.Console
-{
-    internal class BuildConfiguration
-    {
-        [JsonProperty("unity")]
-        public UnityCloudBuildSettings UnitySettings { get; internal set; }
+namespace BuildUploader.Console {
+  internal class BuildConfiguration {
+    [JsonProperty("unity")]
+    public UnityCloudBuildSettings UnitySettings { get; internal set; }
 
-        [JsonProperty("steam")]
-        public SteamSettings SteamSettings { get; internal set; }
-    }
+    [JsonProperty("steam")]
+    public SteamSettings SteamSettings { get; internal set; }
+  }
 }
--- BuildUploader.Console/BuildDefinition.cs
+++ BuildUploader.Console/BuildDefinition.cs
@@ -1,16 +1,13 @@
-﻿namespace BuildUploader.Console
-{
-    internal class BuildDefinition
-    {
-        public int BuildNumber;
+﻿namespace BuildUploader.Console {
+  internal class BuildDefinition {
+    public int BuildNumber;
 
-        public string FileName;
+    public string FileName;
 
-        public string DownloadUrl;
+    public string DownloadUrl;
 
-        public override string ToString()
-        {
-            return string.Format("Build({0})", this.FileName);
-        }
+    public override string ToString() {
+      return string.Format("Build({0})", this.FileName);
     }
+  }
 }
